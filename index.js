457499970a4aed82717bcb4b69f6b1f7374108b0
JPlante9117 <jacrplante@gmail.com>
2020-01-31 11:06:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    let letters = string.split('')
+    letters[0] = letters[0].toUpperCase()
+    string = letters.join("")
+    return string
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string){
+    let lowerWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let words = string.split(" ")
+    words[0] = Formatter.capitalize(words[0])
+    for(let i = 1; i < words.length; i++){
+      if(!lowerWords.includes(words[i])){
+        words[i] = Formatter.capitalize(words[i])
+      }
+    }
+    string = words.join(" ")
+    return string
+  }
 }
